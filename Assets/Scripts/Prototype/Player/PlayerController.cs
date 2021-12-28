a4c9c03dcc9d75c5db7e5663cb2915e634689227
Vladimir TripleA <intfloatbool@gmail.com>
2021-12-28 17:08:38 +0300

    Управление игроком

--- Assets/Scripts/Prototype/Player/PlayerController.cs
+++ Assets/Scripts/Prototype/Player/PlayerController.cs
@@ -7,9 +7,26 @@ namespace Prototype.Player
         [SerializeField] private InputController _inputController;
         private float _moveSpeed;
 
+        private Vector3 _maxMoveBorder;
+        private Vector3 _minMoveBorder;
+
         private void Start()
         {
             _moveSpeed = GameManager.ValuesProvider.GetFloat("PLAYER_MOVE_SPEED");
+
+            _maxMoveBorder = new Vector3(
+                GameManager.ValuesProvider.GetFloat("PLAYER_BORDER_MAX_X"),
+                GameManager.ValuesProvider.GetFloat("PLAYER_BORDER_MAX_Y"),
+                0
+            );
+            
+            _minMoveBorder = new Vector3(
+                GameManager.ValuesProvider.GetFloat("PLAYER_BORDER_MIN_X"),
+                GameManager.ValuesProvider.GetFloat("PLAYER_BORDER_MIN_Y"),
+                0
+            );
+            
+
         }
 
         private void Update()
@@ -21,7 +38,31 @@ namespace Prototype.Player
                     _inputController.Vertical,
                     0
                 );
-                transform.Translate(moveDirection * _moveSpeed * Time.deltaTime);
+
+                var nextPosition = transform.position + moveDirection * _moveSpeed * Time.deltaTime;
+                
+                if (nextPosition.x > _maxMoveBorder.x)
+                {
+                    nextPosition.x = _maxMoveBorder.x;
+                }
+
+                if (nextPosition.x < _minMoveBorder.x)
+                {
+                    nextPosition.x = _minMoveBorder.x;
+                } 
+                
+                if (nextPosition.y > _maxMoveBorder.y)
+                {
+                    nextPosition.y = _maxMoveBorder.y;
+                }
+
+                if (nextPosition.y < _minMoveBorder.y)
+                {
+                    nextPosition.y = _minMoveBorder.y;
+                }
+
+
+                transform.position = nextPosition;
             }
             else
             {
