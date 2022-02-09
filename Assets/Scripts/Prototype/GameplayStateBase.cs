c5e4c5c3b2d181be37532050f4117ec573f053f8
Vladimir TripleA <intfloatbool@gmail.com>
2022-02-09 16:32:30 +0300

    custom params base

--- Assets/Scripts/Prototype/GameplayStateBase.cs
+++ Assets/Scripts/Prototype/GameplayStateBase.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+
 namespace Prototype
 {
     public abstract class GameplayStateBase
     {
+        private Dictionary<string, string> _parameters;
+        public virtual void SetParams(Dictionary<string, string> parameters)
+        {
+            _parameters = parameters;
+        } 
         public abstract void OnStateStarted();
         public abstract void Update();
         public abstract void OnStateStopped();
