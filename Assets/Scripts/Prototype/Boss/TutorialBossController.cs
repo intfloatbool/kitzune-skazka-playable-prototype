63bad8baf321127b70a9d9e9854f5b2463888427
Vladimir TripleA <intfloatbool@gmail.com>
2022-02-16 09:57:15 +0300

    Boss tutorial #2

--- Assets/Scripts/Prototype/Boss/TutorialBossController.cs
+++ Assets/Scripts/Prototype/Boss/TutorialBossController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using BezierSolution;
+using DG.Tweening;
 using Prototype.GameUI;
 using Prototype.Player;
 using UnityEngine;
@@ -8,6 +9,11 @@ namespace Prototype.Boss
 {
     public class TutorialBossController : MonoBehaviour
     {
+        [SerializeField] private GameObject _closedEye;
+        [SerializeField] private GameObject _bossGo;
+        [SerializeField] private GameObject _openedEye;
+        [SerializeField] private GameObject _hands;
+        [SerializeField] private GameObject _soul;
         private IEnumerator Start()
         {
             
@@ -16,7 +22,10 @@ namespace Prototype.Boss
             var playerController = FindObjectOfType<PlayerController>();
 
             var soulMover = boss.Soul.GetComponent<BezierWalkerWithSpeed>();
-
+            _closedEye.SetActive(true);
+            _bossGo.transform.localScale = Vector3.zero;
+            _soul.SetActive(true);
+            _soul.transform.localScale = Vector3.zero;
             yield return new WaitForEndOfFrame();
             
             soulMover.SetActive(false);
@@ -26,9 +35,26 @@ namespace Prototype.Boss
             
             yield return new WaitForSeconds(1f);
             
+            _closedEye.SetActive(false);
+            _openedEye.SetActive(true);
+            _hands.SetActive(true);
+            var handsPos = _hands.transform.position;
+            handsPos.y = -1.5f;
+            _hands.transform.DOMove(handsPos, 1.4f);
+            
+            yield return new WaitForEndOfFrame();
+            
+            yield return new WaitForSeconds(1f);
+            _soul.transform.DOScale(Vector3.one * 0.17f, 1f);
+            yield return new WaitForSeconds(1f);
+
             tutorialWindow.ShowTutorial();
             tutorialWindow.OnHide = () =>
             {
+                _soul.SetActive(false);
+                _openedEye.SetActive(false);
+                _hands.gameObject.SetActive(false);
+                _bossGo.transform.localScale = Vector3.one;
                 boss.SetActive(true);
                 boss.Animator.enabled = true;
                 playerController.SetActive(true);
